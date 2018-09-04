3c87c2faa20c747612501f72fe1dd4565b923ed5
souperogie <souperogie@gmail.com>
2018-09-04 20:03:23 +0800

    Minor changes

--- application/views/pages/gallery.php
+++ application/views/pages/gallery.php
@@ -6,48 +6,31 @@
 			<div style="display: flex; flex-flow: row wrap">
 				<div class="col-md-4">
 					<div class="thumbnail">
-							<a href="assets/img/g1.jpg" class="fresco" data-fresco-group="a1"><img src="assets/img/g1.jpg"></a>
+							<a href="assets/img/g1.jpg" class="fresco" data-fresco-group="a1"><img src="assets/img/g1.jpg" height="450" width="350"></a>
 							<a href="assets/img/g2.jpg" class="fresco" data-fresco-group="a1"></a>
 							<a href="assets/img/g3.jpg" class="fresco" data-fresco-group="a1"></a>
 							<a href="assets/img/g4.jpg" class="fresco" data-fresco-group="a1"></a>
 							<a href="assets/img/g5.jpg" class="fresco" data-fresco-group="a1"></a>
 							<div class="caption">
-								<p style="font-size: 20px; text-align: center;"><strong>A sample Album.</strong></p>
-								<p><b>Event:</b></p>
+								<!-- <p style="font-size: 20px; text-align: center;"><strong>A sample Album.</strong></p> -->
+								<p><b>Event: </b>Review Sessions</p>
 								<p><b>Date:</b></p>
-								<p><b>Place:</b></p>
+								<p><b>Place: </b>Prism CPA Review Center</p>
 							</div>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="thumbnail">
-							<a href="assets/img/g1.jpg" class="fresco" data-fresco-group="a2"><img src="assets/img/g1.jpg"></a>
-							<a href="assets/img/g2.jpg" class="fresco" data-fresco-group="a2"></a>
-							<a href="assets/img/g3.jpg" class="fresco" data-fresco-group="a2"></a>
-							<a href="assets/img/g4.jpg" class="fresco" data-fresco-group="a2"></a>
-							<a href="assets/img/g5.jpg" class="fresco" data-fresco-group="a2"></a>
+							<a href="assets/img/b1.jpg" class="fresco" data-fresco-group="b1"><img src="assets/img/b1.jpg" height="400" width="350"></a>
+							<a href="assets/img/b2.jpg" class="fresco" data-fresco-group="b1"></a>
+							<a href="assets/img/b3.jpg" class="fresco" data-fresco-group="b1"></a>
+							<a href="assets/img/b4.jpg" class="fresco" data-fresco-group="b1"></a>
+							<a href="assets/img/b5.jpg" class="fresco" data-fresco-group="b1"></a>
 							<div class="caption">
-								<p style="font-size: 20px; text-align: center;"><strong>A sample Album.</strong></p>
-								<p><b>Event:</b></p>
+								<p><b>Event:</b> First CPD Seminar at Biñan</p>
 								<p><b>Date:</b></p>
-								<p><b>Place:</b></p>
-							</div>
-					</div>
-				</div>
-
-				<div class="col-md-4">
-					<div class="thumbnail">
-							<a href="assets/img/g1.jpg" class="fresco" data-fresco-group="a3"><img src="assets/img/g1.jpg"></a>
-							<a href="assets/img/g2.jpg" class="fresco" data-fresco-group="a3"></a>
-							<a href="assets/img/g3.jpg" class="fresco" data-fresco-group="a3"></a>
-							<a href="assets/img/g4.jpg" class="fresco" data-fresco-group="a3"></a>
-							<a href="assets/img/g5.jpg" class="fresco" data-fresco-group="a3"></a>
-							<div class="caption">
-								<p style="font-size: 20px; text-align: center;"><strong>A sample Album.</strong></p>
-								<p><b>Event:</b></p>
-								<p><b>Date:</b></p>
-								<p><b>Place:</b></p>
+								<p><b>Place:</b> Prism CPA Review Center</p>
 							</div>
 					</div>
 				</div>
